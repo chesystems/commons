2ce601a2cee6c29590bb4da1dda8f1b42cd32ea0
chesystems <chesystemsdev@gmail.com>
2024-11-17 17:30:18 -0300

    EZAnimatedColumn

---
+++ uibits/src/main/java/com/chesystems/uibits/EZList.kt
@@ -0,0 +1,35 @@
+package com.chesystems.uibits
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.ui.Modifier
+
+@Composable
+fun <T> EZAnimatedColumn(
+    items: List<T>,
+    contentPadding: PaddingValues,
+    onEmpty: @Composable () -> Unit,
+    itemKey: (T) -> Any,
+    itemContent: @Composable (T, Modifier) -> Unit,
+    finalContent: @Composable (() -> Unit)? = null
+) {
+    if (items.isNotEmpty()) {
+        LazyColumn(
+            contentPadding = contentPadding
+        ) {
+            items(
+                items = items,
+                key = { itemKey(it) }
+            ) { item ->
+                key(itemKey(item)) {
+                    itemContent(item, Modifier.animateItem())
+                }
+            }
+            if(finalContent != null)
+                item { finalContent() }
+        }
+    } else onEmpty()
+}
